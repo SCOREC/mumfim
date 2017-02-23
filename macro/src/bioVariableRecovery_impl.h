06156a3358ac01d1a8137ed0d482cd8022d5ef75
William Tobin <tobinw2@rpi.edu>
2017-02-23 09:55:09 -0500

    updating implementation used for variable recovery of field components to allow recovery of multiple components, as well as updating the names and function signatures to operate on iterators rather than specific containers, also using the new SimIterators to iterate over classified mesh entities in lieu of collecting a container of mesh entities

--- macro/src/bioVariableRecovery_impl.h
+++ macro/src/bioVariableRecovery_impl.h
@@ -1,13 +1,54 @@
+#include "simClassified.h"
+#include "apfField.h"
+#include <cassert>
 namespace bio
 {
-  template <typename T>
-    void getEntsFieldComponent(apf::Field * fld,
-                               int crd,
-                               std::vector<double>& crds,
-                               T begin,
-                               T end)
+  template <typename O>
+    void getFieldComponentsClassified(apf::Field * fld,
+                                      apf::ModelEntity * mdl_ent,
+                                      int nm_cmps,
+                                      int * cmps,
+                                      O out)
   {
-    for(T it = begin; it != end; it++)
-      getEntFieldComponent(fld,crd,crds,*it);
+    for(int ent_dim = 0; ent_dim < 3; ent_dim++)
+    {
+      amsi::SimIterator * bgn = amsi::beginClassified(apf::getMesh(fld),
+                                                      mdl_ent,
+                                                      ent_dim);
+      amsi::SimIterator * end = amsi::endClassified(bgn);
+      getFieldComponentsOn(fld,nm_cmps,cmps,bgn,end,out);
+    }
+  }
+  template <typename I, typename O>
+    void getFieldComponentsOn(apf::Field * fld,
+                              int nm_cmps,
+                              int * cmps,
+                              I bgn,
+                              I end,
+                              O out)
+  {
+    for(I it = bgn; it != end; it++)
+      getEntFieldComponents(fld,*it,nm_cmps,cmps,out);
+  }
+  template <typename O>
+    void getFieldComponentsOnEnt(apf::Field * fld,
+                                 apf::MeshEntity * ent,
+                                 int nm_cmps,
+                                 int * cmps,
+                                 O out)
+  {
+    int cnt = fld->countNodesOn(ent);
+    for(int nde = 0; nde < cnt; nde++)
+    {
+      int fld_cmps = apf::countComponents(fld);
+      assert(nm_cmps < fld_cmps);
+      double fld_data[fld_cmps];
+      apf::getComponents(fld,ent,nde,fld_data);
+      for(int ii = 0; ii < nm_cmps; ++ii)
+      {
+        assert(cmps[ii] < fld_cmps);
+        *out++ = fld_data[cmps[ii]];
+      }
+    }
   }
 }
