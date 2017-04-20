21e3169f484cfc0bfb54d6b86ffcc0ee333d0d67
William Tobin <tobinw2@rpi.edu>
2017-04-20 15:07:29 -0400

    switching to gmi_ent instead of simmetrix model entities, and removing accidental parenthesis in CMakelists.txt

--- macro/src/bioStiffnessVariation.cc
+++ macro/src/bioStiffnessVariation.cc
@@ -10,58 +10,34 @@ namespace bio
   StiffnessVariation * buildStiffnessVariation(pACase cs, pANode nd, apf::Field * stf_vrtn_fld)
   {
     StiffnessVariation * stfvar = NULL;
-
     /* cs = "problem definition" in simmodeler.
        nd = "stiffness gradient" in simmodeler. nd has three children.
        - "source"   : of type pAttInfoRefNode
        - "sink"     : of type pAttInfoRefNode
        - "function" : expression
      */
-
     amsi::describeNode(nd);
-    
     pANode src_AttRefNode, snk_AttRefNode;
     src_AttRefNode = AttNode_childByType(nd,"sources");
     snk_AttRefNode = AttNode_childByType(nd,"sinks");
     amsi::describeNode(src_AttRefNode);
-    
     pANode src_nd, snk_nd;
     src_nd = AttInfoRefNode_value((pAttInfoRefNode)src_AttRefNode);
     snk_nd = AttInfoRefNode_value((pAttInfoRefNode)snk_AttRefNode);
     amsi::describeNode(src_nd);
-    
-    std::vector<pModelItem> mdl_src_itms;
     std::vector<apf::ModelEntity*> mdl_src_ents;
-    amsi::getAssociatedModelItems(cs,src_nd,std::back_inserter(mdl_src_itms));
-    std::transform(mdl_src_itms.begin(),mdl_src_itms.end(),std::back_inserter(mdl_src_ents),amsi::reinterpret_caster<pModelItem,apf::ModelEntity*>());
-    /* New implementation
-       std::vector<apf::ModelEntity*> mdl_src_ents;
-       amsi::getAssociatedModelItems(cs,src_nd,std::back_inserter(mdl_src_ents));
-     */
+    amsi::getAssociatedModelItems(cs,src_nd,std::back_inserter(mdl_src_ents));
     std::cout << "Stiffness sources include model entities : ";
-    for (auto mdl_ent = mdl_src_itms.begin(); mdl_ent != mdl_src_itms.end(); ++mdl_ent)
-    {
-      assert(ModelItem_isGEntity(*mdl_ent));
-      std::cout << ModelItem_tag(*mdl_ent) << " ";
-    }
+    gmi_model * mdl = apf::getMesh(stf_vrtn_fld)->getModel();
+    for (auto mdl_ent = mdl_src_ents.begin(); mdl_ent != mdl_src_ents.end(); ++mdl_ent)
+      std::cout << mdl->ops->tag(mdl,(gmi_ent*)*mdl_ent) << " ";
     std::cout << std::endl;
-
-    std::vector<pModelItem> mdl_snk_itms;
     std::vector<apf::ModelEntity*> mdl_snk_ents;
-    amsi::getAssociatedModelItems(cs,snk_nd,std::back_inserter(mdl_snk_itms)); 
-    std::transform(mdl_snk_itms.begin(),mdl_snk_itms.end(),std::back_inserter(mdl_snk_ents),amsi::reinterpret_caster<pModelItem,apf::ModelEntity*>());
-    /* New implementation
-       std::vector<apf::ModelEntity*> mdl_snk_ents;
-       amsi::getAssociatedModelItems(cs,snk_nd,std::back_inserter(mdl_snk_ents));
-     */
+    amsi::getAssociatedModelItems(cs,snk_nd,std::back_inserter(mdl_snk_ents));
     std::cout << "Stiffness sinks include model entities : ";
-    for (auto mdl_ent = mdl_snk_itms.begin(); mdl_ent != mdl_snk_itms.end(); ++mdl_ent)
-    {
-      assert(ModelItem_isGEntity(*mdl_ent));
-      std::cout << ModelItem_tag(*mdl_ent) << " ";
-    }
+    for (auto mdl_ent = mdl_snk_ents.begin(); mdl_ent != mdl_snk_ents.end(); ++mdl_ent)
+      std::cout << mdl->ops->tag(mdl,(gmi_ent*)*mdl_ent) << " ";
     std::cout << std::endl;
-
     pANode fn_nd;
     fn_nd = AttNode_childByType(nd,"function");
     stfvar = new StiffnessVariation(mdl_src_ents.begin(), mdl_src_ents.end(), mdl_snk_ents.begin(), mdl_snk_ents.end(), stf_vrtn_fld, fn_nd);
@@ -85,40 +61,38 @@ namespace bio
       gmi_set * adj_mdl_snk_fcs = mdl->ops->adjacent(mdl,(gmi_ent*)(*snk),2);
       for (int ii = 0; ii < adj_mdl_snk_fcs->n; ++ii)
       {
-	gmi_ent * mdl_snk_fc = adj_mdl_snk_fcs->e[ii];
-	/* Find closest point to model face entity mdl_src_fc. */
-	auto bgn = amsi::beginClassified(msh,(apf::ModelEntity*)mdl_snk_fc,0); // vertex entities classified on model face entity.
-	auto end = amsi::endClassified(bgn);
-	double fc_xyz[3]={0.0};
-	amsi::getAvgFieldValue(msh->getCoordinateField(),bgn,end,fc_xyz);
-
-	/* Implementation if gmi_closest_point is working.   
-	   double mdl_src_fc_uv[2] = {0};
-	   std::cout<<"mdl_src_fc is"<<gmi_tag(msh->getModel(),(gmi_ent*)mdl_src_fc)<<std::endl;
-	   gmi_closest_point(msh->getModel(),(gmi_ent*)mdl_src_fc,gp,mdl_src_fc_xyz,mdl_src_fc_uv);
-	*/
-
-	for (auto src = mdl_src_ents.begin(); src != mdl_src_ents.end(); ++src)
-	{	  
-	  /* if adjacent model sink face is in closure of model source region.
-	     - mdl        : type gmi_model*.
-	     - mdl_src    : type apf::ModelEntity **.
-	     - mdl_snk_fc : type gmi_ent *.
-	   */
-	  std::cout<<"face "<<gmi_tag(mdl,mdl_snk_fc)<<" in model region "<<gmi_tag(mdl,(gmi_ent*)(*src))<<"?"<<std::endl;
-	  if (gmi_is_in_closure_of(mdl,mdl_snk_fc,(gmi_ent*)(*src)))
-	  {
-	    std::cout<<"adj. snk model face is "<<gmi_tag(mdl,mdl_snk_fc)<<std::endl;
-	    std::cout<<"adj. snk model face coord. is "<<fc_xyz[0]<<","<<fc_xyz[1]<<","<<fc_xyz[2]<<std::endl;	    
-	    for (auto msh_rgn = amsi::beginClassified(msh,*snk,dm); msh_rgn != amsi::endClassified(msh_rgn); ++msh_rgn)
-	    {
-	      this->set_mdl_fc_xyz(fc_xyz);
-	      this->inElement(*msh_rgn);
-	      apf::MeshElement * msh_elmt = apf::createMeshElement(msh,*msh_rgn);
-	      this->process(msh_elmt);
-	    }
-	  }
-	}
+        gmi_ent * mdl_snk_fc = adj_mdl_snk_fcs->e[ii];
+        /* Find closest point to model face entity mdl_src_fc. */
+        auto bgn = amsi::beginClassified(msh,(apf::ModelEntity*)mdl_snk_fc,0); // vertex entities classified on model face entity.
+        auto end = amsi::endClassified(bgn);
+        double fc_xyz[3]={0.0};
+        amsi::getAvgFieldValue(msh->getCoordinateField(),bgn,end,fc_xyz);
+        /* Implementation if gmi_closest_point is working.
+           double mdl_src_fc_uv[2] = {0};
+           std::cout<<"mdl_src_fc is"<<gmi_tag(msh->getModel(),(gmi_ent*)mdl_src_fc)<<std::endl;
+           gmi_closest_point(msh->getModel(),(gmi_ent*)mdl_src_fc,gp,mdl_src_fc_xyz,mdl_src_fc_uv);
+        */
+        for (auto src = mdl_src_ents.begin(); src != mdl_src_ents.end(); ++src)
+        {
+          /* if adjacent model sink face is in closure of model source region.
+             - mdl        : type gmi_model*.
+             - mdl_src    : type apf::ModelEntity **.
+             - mdl_snk_fc : type gmi_ent *.
+           */
+          std::cout<<"face "<<gmi_tag(mdl,mdl_snk_fc)<<" in model region "<<gmi_tag(mdl,(gmi_ent*)(*src))<<"?"<<std::endl;
+          if (gmi_is_in_closure_of(mdl,mdl_snk_fc,(gmi_ent*)(*src)))
+          {
+            std::cout<<"adj. snk model face is "<<gmi_tag(mdl,mdl_snk_fc)<<std::endl;
+            std::cout<<"adj. snk model face coord. is "<<fc_xyz[0]<<","<<fc_xyz[1]<<","<<fc_xyz[2]<<std::endl;
+            for (auto msh_rgn = amsi::beginClassified(msh,*snk,dm); msh_rgn != amsi::endClassified(msh_rgn); ++msh_rgn)
+            {
+              this->set_mdl_fc_xyz(fc_xyz);
+              this->inElement(*msh_rgn);
+              apf::MeshElement * msh_elmt = apf::createMeshElement(msh,*msh_rgn);
+              this->process(msh_elmt);
+            }
+          }
+        }
       }
     }
   }
@@ -131,15 +105,13 @@ namespace bio
     apf::mapLocalToGlobal(apf::createMeshElement(msh,msh_ent),p,gauss_pt_xyz);
     double gp[3] = {0};
     gauss_pt_xyz.toArray(gp);
-
     double dist = 0.0;
     for (auto i=0; i<dm; ++i)
       dist += (gp[i] - mdl_fc_xyz[i]) * (gp[i] - mdl_fc_xyz[i]);
     dist = std::sqrt(dist);
     // The expression from pANode fn is assumed to have the form 1-dist/x, where x is specified in simmodeler.
-    double x_val = 30.0; //assume variation distance is 10 microns.    
+    double x_val = 30.0; //assume variation distance is 10 microns.
     apf::setScalar(stf_vrtn_fld,msh_ent,ip_integration_pt,1.0-dist/x_val);
     ip_integration_pt++;
   }
-  
 }
