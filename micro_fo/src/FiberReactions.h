2c40dc2c1523cf3af2412597f0ea4d026a046246
Victor Chan <chanv3@mastermind.scorec.rpi.edu>
2017-05-10 16:37:46 -0400

    increase stiffness in compression for nonlinear fiber force.

--- micro_fo/src/FiberReactions.h
+++ micro_fo/src/FiberReactions.h
@@ -24,21 +24,31 @@ struct NonlinearReaction : public FiberReaction
     // Reminder: Every time you change E you have to change the derivatives of the forces too. See gfunc.c
     double length_ratio = length / orig_length;
     double tension = E * fiber_area / B;
-    if(length_ratio < length_ratio_trns)
-    {
-      double green_strain = 0.5 * (length_ratio * length_ratio - 1.0);
-      double expBeps = exp(B * green_strain);
-      result.first = tension * (expBeps - 1.0);
-      result.second = ((fiber_area * E * length) / (orig_length * orig_length)) * expBeps;
-    }
-    else
+    double cmp_ratio_trns = 1.0;
+    if(length_ratio > length_ratio_trns)
     {
       double green_strain_trns = 0.5 * (length_ratio_trns * length_ratio_trns - 1.0);
       double expBeps_trns = exp(B * green_strain_trns);
       double m = fiber_area * E * length_ratio_trns * expBeps_trns; // slope
       double b = tension * (expBeps_trns - 1.0) - m * length_ratio_trns; // y-intercept 
       result.first = m * length_ratio + b;
+      result.second = m / orig_length;
+    }
+    else if(length_ratio < cmp_ratio_trns)
+    {
+      double green_strain_trns = 0.5 * (cmp_ratio_trns * cmp_ratio_trns - 1.0);
+      double expBeps_trns = exp(B * green_strain_trns);
+      double m = fiber_area * E * cmp_ratio_trns * expBeps_trns; // slope
+      double b = tension * (expBeps_trns - 1.0) - m * cmp_ratio_trns; // y-intercept 
+      result.first = m * length_ratio + b;
       result.second = m / orig_length;      
+    }
+    else
+    {
+      double green_strain = 0.5 * (length_ratio * length_ratio - 1.0);
+      double expBeps = exp(B * green_strain);
+      result.first = tension * (expBeps - 1.0);
+      result.second = ((fiber_area * E * length) / (orig_length * orig_length)) * expBeps;
     }
     return result;
   }
