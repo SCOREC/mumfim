befdba30cacad3cb732219761a702dc2c94d8554
William Tobin <tobinw2@rpi.edu>
2017-05-12 14:55:46 -0400

    accomodating upstream change in amsi, using ModularIteration instead of just Iteration

--- macro/src/bioTissueAnalysis.h
+++ macro/src/bioTissueAnalysis.h
@@ -9,14 +9,14 @@ namespace bio
     void buildLASConvergenceOperators(pACase ss, amsi::Iteration * it, amsi::LAS * las, O out);
   template <typename I, typename O>
     void buildVolumeConvergenceOperators(pACase ss, amsi::Iteration * it, I vl_tks, apf::Field * u, O out);
-  class TissueIteration : public amsi::Iteration
+  class TissueIteration : public amsi::ModularIteration
   {
   protected:
     NonlinearTissue * tssu;
     amsi::LAS * las;
   public:
     TissueIteration(NonlinearTissue * t, amsi::LAS * l)
-      : amsi::Iteration()
+      : amsi::ModularIteration()
       , tssu(t)
       , las(l)
     {}
@@ -25,7 +25,7 @@ namespace bio
       LinearSolver(tssu,las);
       tssu->iter();
       las->iter();
-      amsi::Iteration::iterate();
+      amsi::ModularIteration::iterate();
     }
   };
   class TissueAnalysis
