4509f2a18d669668f1c7eb2888f5be400dc19040
Jacob Merson <jacob@jacobmerson.com>
2017-06-28 02:50:53 -0400

    Updates to Neohookean Integrator (#57)
    * Updated neohookean integrator
    
    1) Uses Green Lagrange Strain/Cauchy Stress
    2) Uses new apf functionality to simplify computations
    
    * applied formatting

--- macro/src/bioNonlinearTissue.h
+++ macro/src/bioNonlinearTissue.h
@@ -1,66 +1,91 @@
 #ifndef BIO_NONLINEAR_TISSUE_H_
 #define BIO_NONLINEAR_TISSUE_H_
-#include "bioLinearTissue.h"
-#include "MicroFOMultiscaleTypes.h"
-#include "RepresentVolElem.h"       // should be able to take this out... (needed for RVE_Info struct)
-#include "bioVolumeConstraint.h"
-#include "bioStiffnessVariation.h"
-#include <apfFEA.h>
-#include <Solvers.h>
 #include <NonLinElasticity.h>
-#include <amsiMultiscale.h>
+#include <Solvers.h>
 #include <amsiAnalysis.h>
+#include <amsiMultiscale.h>
 #include <amsiUtil.h>
+#include <apfFEA.h>
 #include <apfsimWrapper.h>
-#include <vector>
+#include <iostream>
 #include <list>
 #include <string>
-#include <iostream>
-namespace bio
-{
-  class NonlinearTissue : public amsi::apfSimFEA
-  {
-  protected:
-    std::map<pGEntity,amsi::ElementalSystem*> constitutives;
+#include <vector>
+#include "MicroFOMultiscaleTypes.h"
+#include "RepresentVolElem.h"  // should be able to take this out... (needed for RVE_Info struct)
+#include "bioLinearTissue.h"
+#include "bioStiffnessVariation.h"
+#include "bioVolumeConstraint.h"
+namespace bio {
+  class CurrentCoordFunc;
+  class NonlinearTissue : public amsi::apfSimFEA {
+    protected:
+    std::map<pGEntity, amsi::ElementalSystem*> constitutives;
     std::vector<StiffnessVariation*> stf_vrtn_cnst;
     std::vector<VolumeConstraint*> vol_cnst;
-    apf::Field * delta_u;
-    apf::Field * strs;
-    apf::Field * rcvrd_strs;
-    apf::Field * strn;
-    apf::Field * det_dfm_grd;
-    apf::Field * previous_rve;
-    apf::Field * stf_vrtn;
-    apf::Field * axl_yngs_mod;
+    apf::Field* delta_u;
+    apf::Field* current_coords;  // coordinates in current config
+    apf::Field* strs;
+    apf::Field* rcvrd_strs;
+    apf::Field* strn;
+    apf::Field* det_dfm_grd;
+    apf::Field* previous_rve;
+    apf::Field* stf_vrtn;
+    apf::Field* axl_yngs_mod;
     double dv_prev;
     int load_step;
     int iteration;
-  public:
-    NonlinearTissue(pGModel imdl,
-                 pParMesh imsh,
-                 pACase pd,
-                 MPI_Comm cm = AMSI_COMM_SCALE);
+
+    public:
+    NonlinearTissue(pGModel imdl, pParMesh imsh, pACase pd,
+                    MPI_Comm cm = AMSI_COMM_SCALE);
     virtual ~NonlinearTissue();
-    void computeInitGuess(amsi::LAS * las);
+    void computeInitGuess(amsi::LAS* las);
     virtual void ApplyBC_Dirichlet();
-    void getLoadOn(pGEntity ent, double * frc);
+    void getLoadOn(pGEntity ent, double* frc);
     void step();
     void iter();
-    virtual void Assemble(amsi::LAS * las);
-    virtual void UpdateDOFs(const double * );
+    virtual void Assemble(amsi::LAS* las);
+    virtual void UpdateDOFs(const double*);
     void UpdateLambda();
     void UpdateBeta(double);
     void setBeta(double);
     void setLambda(double);
-    void computeDispL2Norm(double &);
+    void computeDispL2Norm(double&);
     void recoverSecondaryVariables(int);
-    void storeStress(apf::MeshElement * me, double * stress);
-    void storeStrain(apf::MeshElement * me, double * strain);
-    int getIteration(){return iteration;}
-    apf::Numbering * getNumbering() { return apf_primary_numbering; }
-    apf::Field * getdUField() { return delta_u; }
-    apf::Field * getUField() {return apf_primary_field;}
-    //void logCnstrntParams(int ldstp, int iteration, int rnk);
+    void storeStress(apf::MeshElement* me, double* stress);
+    void storeStress(apf::MeshElement* me, apf::Matrix3x3 eps);
+    void storeStrain(apf::MeshElement* me, double* strain);
+    void storeStrain(apf::MeshElement* me, apf::Matrix3x3 eps);
+    int getIteration() { return iteration; }
+    apf::Numbering* getNumbering() { return apf_primary_numbering; }
+    apf::Field* getdUField() { return delta_u; }
+    apf::Field* getUField() { return apf_primary_field; }
+    apf::Mesh* getMesh() { return apf_mesh; }
+    CurrentCoordFunc* currentCoordFunc;
+    // void logCnstrntParams(int ldstp, int iteration, int rnk);
+    // function to get mapping by summing reference coordinate with
+    // displacements
+  };
+  class CurrentCoordFunc : public apf::Function {
+    public:
+    CurrentCoordFunc(NonlinearTissue* nlt) : nlt(nlt) {}
+    void eval(apf::MeshEntity* e, double* result)
+    {
+      // make sure that we are only evaluating on vertices
+      assert(nlt->getMesh()->getType(e) == apf::Mesh::VERTEX);
+      apf::Vector3 X, U;
+      // get the displacements
+      apf::getVector(nlt->getUField(), e, 0, U);
+      // get the reference coordinates
+      apf::getVector(nlt->getMesh()->getCoordinateField(), e, 0, X);
+      apf::Vector3* r = (apf::Vector3*)result;
+      // set the current coordinates to be the reference plus the displacements
+      *r = X + U;
+    }
+
+    private:
+    NonlinearTissue* nlt;
   };
 }
 #endif
