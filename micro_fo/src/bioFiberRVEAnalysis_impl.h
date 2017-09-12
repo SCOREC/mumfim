3530f4119c27fb36492fb0584e13abed5e563c0f
William Tobin <tobinw2@rpi.edu>
2017-09-12 12:47:00 -0400

    just FIX boundary nodes to remove them from the linear system, don't leave them in there, also suppress some warnings which come about as a result of this

--- micro_fo/src/bioFiberRVEAnalysis_impl.h
+++ micro_fo/src/bioFiberRVEAnalysis_impl.h
@@ -3,8 +3,14 @@ namespace bio
   template <typename I>
     void applyRVEForceBC(I bnd_bgn, I bnd_end, apf::Numbering * nm, las::LasOps * ops, las::Vec * f, las::Mat * k)
   {
+    (void)ops;
+    (void)f;
+    (void)k;
     for(auto ent = bnd_bgn; ent != bnd_end; ++ent)
     {
+      for(int ii = 0; ii < 3; ++ii)
+        apf::fix(nm,*ent,0,ii,true);
+      /*
       apf::NewArray<int> dofs;
       int nedofs = apf::getElementNumbers(nm,*ent,dofs);
       apf::DynamicVector zeroes(nedofs);
@@ -16,6 +22,7 @@ namespace bio
         for(int jj = 0; jj < nedofs; ++jj)
           eye(ii,jj) = 1.0;
       ops->set(k,nedofs,&dofs[0],nedofs,&dofs[0],&eye(0,0));
+      */
     }
   }
 }
