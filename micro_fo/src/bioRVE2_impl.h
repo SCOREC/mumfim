60f7bd6d3569a88c447d9025a86b112545cade16
William Tobin <tobinw2@rpi.edu>
2017-10-26 16:03:52 -0400

    simplify/correct getBoundaryVerts

--- micro_fo/src/bioRVE2_impl.h
+++ micro_fo/src/bioRVE2_impl.h
@@ -3,23 +3,16 @@
 namespace bio
 {
   template <typename O>
-    void getBoundaryVerts(RVE * rve, FiberNetwork * fn, RVE::side sd, O nds)
+    void getBoundaryVerts(const RVE * rve, apf::Mesh * fn_msh, RVE::side sd, O nds)
   {
-    apf::Mesh * fn_msh = fn->getNetworkMesh();
-    apf::Field * u = fn->getUField();
-    apf::FieldShape * shp = apf::getShape(u);
     apf::MeshEntity * me = NULL;
     apf::MeshIterator * it = NULL;
     for(it = fn_msh->begin(0); (me = fn_msh->iterate(it)) ; )
     {
-      int cnt = shp->countNodesOn(fn_msh->getType(me));
-      for(int nd = 0; nd < cnt; nd++)
-      {
-        apf::Vector3 crd;
-        fn_msh->getPoint(me,nd,crd);
-        if(rve->onBoundary(crd,sd))
-          *nds++ = me;
-      }
+      apf::Vector3 crd;
+      fn_msh->getPoint(me,0,crd);
+      if(rve->onBoundary(crd,sd))
+        *nds++ = me;
     }
     fn_msh->end(it);
   }
