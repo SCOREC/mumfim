a2f1743e7756c1399acaf66de16a2ff0d6bc1859
Jacob Merson <mersoj@rpi.edu>
2018-06-29 12:54:51 -0400

    fix bug in conversion script
    run_convert_network now properly names new networks

--- micro_fo/scripts/run_convert_network.m
+++ micro_fo/scripts/run_convert_network.m
@@ -1,15 +1,16 @@
 clear, clc;
 % converts networks and generate parameter files
-d = '/fasttmp/mersoj/develop/biotissue_problems/fiber_networks/density_100_networks/';
+d = '/fasttmp/deoges/develop/bio_probs/fiber_networks/set_4/';
 files = dir(fullfile(d, '*.txt'));
 
-names = {files.name}
-for i=1:size(names,2)
-  names{i}
+names = {files.name};
+%for i=1:size(names,2)
+for i=1:7
+  names{i};
   % append new to the name before the numeral
-  underscores = strfind(names{1},'_');
+  underscores = strfind(names{i},'_');
   lstUnd = underscores(end);
-  new_name = strcat(names{i}(1:lstUnd), 'new', names{i}(lstUnd:end));
+  new_name = strcat(names{i}(1:lstUnd), 'new', names{i}(lstUnd:end))
   convert_network(names{i}, new_name);
-  generate_parameters(new_name, 3.846494e-15, 10000);
+  generate_parameters(new_name, 5E-5, 1E8);
 end
