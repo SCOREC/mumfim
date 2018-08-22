a70e5b93c5048b3561be8ba1cb8215682a4f9b23
Jacob Merson <mersoj@rpi.edu>
2018-08-22 16:58:06 -0400

    move ParamLoader class to impl file
    The ParamLoader class was in the header file due to templates, therefore
    it was moved to an impl file to clean up the FiberNetwokIO header.

--- micro_fo/src/bioFiberNetworkIO.h
+++ micro_fo/src/bioFiberNetworkIO.h
@@ -1,91 +1,15 @@
 #ifndef BIO_FIBER_NETWORK_IO_H_
 #define BIO_FIBER_NETWORK_IO_H_
-#include "bioFiberNetwork.h"
-#include "bioFiberReactions.h"
-#include <iostream>
 #include <apf.h>
 #include <apfMesh2.h>
-#include <cmath>
+#include <iostream>
+#include "bioFiberReactions.h"
 namespace bio
 {
   apf::Mesh2 * loadFromStream(std::istream & strm);
   apf::Mesh2 * loadFromFile(const std::string & fnm);
   template <typename O>
-    void loadParamsFromFile(apf::Mesh2 * msh, const std::string & fnm, O rctns);
-  /**
-   * Only here because templates
-   */
-  class ParamLoader
-  {
-  protected:
-    apf::Mesh2 * msh;
-    apf::MeshTag * id_tg;
-    apf::MeshTag * rct_tg;
-    std::vector<FiberReaction*> rctns;
-    std::map<int,int> fbr_2_rctn;
-    template <typename O>
-      void processReactionLine(std::istream & strm, O out)
-    {
-      int tp = -1;
-      strm >> tp;
-      if(tp == FiberConstitutive::linear)
-      {
-        LinearReaction * rct = new LinearReaction;
-        strm >> rct->fiber_area >> rct->E;
-        *out++ = rct;
-      }
-      else if(tp == FiberConstitutive::nonlinear)
-      {
-        NonlinearReaction * rct = new NonlinearReaction;
-        double fiber_radius;
-        strm >> fiber_radius >> rct->E >> rct->B >> rct->length_ratio_trns;
-        rct->fiber_area = fiber_radius*fiber_radius*M_PI;
-        *out++ = rct;
-      }
-    }
-    int processEdgeReactionLine(std::istream & strm)
-    {
-      int r = -1;
-      strm >> r;
-      return r;
-    }
-    void applyReactionLabels()
-    {
-      apf::MeshEntity * me = NULL;
-      for(apf::MeshIterator * it = msh->begin(1); (me = msh->iterate(it));)
-      {
-        long id = -1;
-        msh->getLongTag(me,id_tg,&id);
-        long rctn = fbr_2_rctn[id];
-        msh->setLongTag(me,rct_tg,&rctn);
-      }
-    }
-  public:
-    ParamLoader(apf::Mesh2 * m)
-      : msh(m)
-      , id_tg(msh->findTag("id"))
-      , rct_tg(msh->createIntTag("fiber_reaction",1))
-      , rctns()
-      , fbr_2_rctn()
-    { }
-    template <typename O>
-    void fromStream(std::istream & strm, O out)
-    {
-      int nr = -1;
-      int ne = -1;
-      strm >> nr >> ne;
-      assert(ne == apf::countEntitiesOfType(msh,apf::Mesh::EDGE) && "Must have the same number of edges in the mesh as specified in the parameter file");
-      auto rctns_out = std::back_inserter(rctns);
-      // TODO these process functions should verify the number of items
-      // in the line as to not cause errors in strange places if the input files are not correct
-      for(int ii = 0; ii < nr; ++ii)
-        processReactionLine(strm,rctns_out);
-      for(int ii = 0; ii < ne; ++ii)
-        fbr_2_rctn[ii] = processEdgeReactionLine(strm);
-      applyReactionLabels();
-      std::copy(rctns.begin(),rctns.end(),out);
-    }
-  };
-}
+  void loadParamsFromFile(apf::Mesh2 * msh, const std::string & fnm, O rctns);
+}  // namespace bio
 #include "bioFiberNetworkIO_impl.h"
 #endif
--- micro_fo/src/bioFiberNetworkIO_impl.h
+++ micro_fo/src/bioFiberNetworkIO_impl.h
@@ -1,11 +1,90 @@
+#include <apfMesh2.h>
+#include <cmath>
 #include <fstream>
+#include "bioFiberNetwork.h"
 namespace bio
 {
+  class ParamLoader
+  {
+    protected:
+    apf::Mesh2 * msh;
+    apf::MeshTag * id_tg;
+    apf::MeshTag * rct_tg;
+    std::vector<FiberReaction *> rctns;
+    std::map<int, int> fbr_2_rctn;
+    template <typename O>
+    void processReactionLine(std::istream & strm, O out)
+    {
+      int tp = -1;
+      strm >> tp;
+      if (tp == FiberConstitutive::linear)
+      {
+        LinearReaction * rct = new LinearReaction;
+        strm >> rct->fiber_area >> rct->E;
+        *out++ = rct;
+      }
+      else if (tp == FiberConstitutive::nonlinear)
+      {
+        NonlinearReaction * rct = new NonlinearReaction;
+        double fiber_radius;
+        strm >> fiber_radius >> rct->E >> rct->B >> rct->length_ratio_trns;
+        rct->fiber_area = fiber_radius * fiber_radius * M_PI;
+        *out++ = rct;
+      }
+    }
+    int processEdgeReactionLine(std::istream & strm)
+    {
+      int r = -1;
+      strm >> r;
+      return r;
+    }
+    void applyReactionLabels()
+    {
+      apf::MeshEntity * me = NULL;
+      for (apf::MeshIterator * it = msh->begin(1); (me = msh->iterate(it));)
+      {
+        long id = -1;
+        msh->getLongTag(me, id_tg, &id);
+        long rctn = fbr_2_rctn[id];
+        msh->setLongTag(me, rct_tg, &rctn);
+      }
+    }
+
+    public:
+    ParamLoader(apf::Mesh2 * m)
+        : msh(m)
+        , id_tg(msh->findTag("id"))
+        , rct_tg(msh->createIntTag("fiber_reaction", 1))
+        , rctns()
+        , fbr_2_rctn()
+    {
+    }
+    template <typename O>
+    void fromStream(std::istream & strm, O out)
+    {
+      int nr = -1;
+      int ne = -1;
+      strm >> nr >> ne;
+      assert(ne == apf::countEntitiesOfType(msh, apf::Mesh::EDGE) &&
+             "Must have the same number of edges in the mesh as specified in "
+             "the parameter file");
+      auto rctns_out = std::back_inserter(rctns);
+      // TODO these process functions should verify the number of items
+      // in the line as to not cause errors in strange places if the input files
+      // are not correct
+      for (int ii = 0; ii < nr; ++ii)
+        processReactionLine(strm, rctns_out);
+      for (int ii = 0; ii < ne; ++ii)
+        fbr_2_rctn[ii] = processEdgeReactionLine(strm);
+      applyReactionLabels();
+      std::copy(rctns.begin(), rctns.end(), out);
+    }
+  };
   template <typename O>
-    void loadParamsFromFile(apf::Mesh2 * msh, const std::string & fnm, O rctns)
+  void loadParamsFromFile(apf::Mesh2 * msh, const std::string & fnm, O rctns)
   {
     std::ifstream strm(fnm.c_str());
     ParamLoader ldr(msh);
-    ldr.fromStream(strm,rctns);
+    ldr.fromStream(strm, rctns);
   }
-}
+}  // namespace bio
