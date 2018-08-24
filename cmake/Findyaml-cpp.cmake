ce85893824afe2b9d5f7c887a3ee65cdaadd3c88
Jacob Merson <mersoj@rpi.edu>
2018-08-24 12:38:36 -0400

    fix Findyaml-cpp.cmake file

--- cmake/Findyaml-cpp.cmake
+++ cmake/Findyaml-cpp.cmake
@@ -13,13 +13,15 @@ if(yayaml-cpp_FIND_REQUIRED AND yaml-cpp_FIND_QUIETLY)
 endif()
 
 pkg_check_modules(YAML-CPP ${_yaml-cpp_OPTS} yaml-cpp)
+#message(INFO ${yaml-cpp_INCLUDEDIR})
+#message(INFO ${yaml-cpp_INCLUDE_DIRS})
 
-if(yaml-cpp_FOUND)
-  set(yaml-cpp_INCLUDE_DIRS "${YAML-CPP_STATIC_INCLUDE_DIRS};${YAML-CPP_INCLUDE_DIRS}")
+if(YAML-CPP_FOUND)
+  set(yaml-cpp_INCLUDE_DIRS "${YAML-CPP_INCLUDEDIR}")
   #set(yaml-cpp_INCLUDE_DIRS "${YAML-CPP_INCLUDE_DIRS}")
   #set(yaml-cpp_LIBRARIES "${YAML-CPP_STATIC_LDFLAGS};${YAML-CPP_STATIC_LIBRARIES}")
   set(yaml-cpp_LIBRARIES "${YAML-CPP_LDFLAGS};${YAML-CPP_LIBRARIES}")
-endif(yaml-cpp_FOUND)
+endif(YAML-CPP_FOUND)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(yaml-cpp DEFAULT_MSG yaml-cpp_LIBRARIES yaml-cpp_INCLUDE_DIRS)
