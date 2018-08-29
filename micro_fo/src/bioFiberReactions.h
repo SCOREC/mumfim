e1cf26d3bfd1a245dbee08e204c88e07528013d7
Jacob Merson <mersoj@rpi.edu>
2018-08-28 23:55:35 -0400

    move fiber reaction implementation to cc file

--- micro_fo/src/bioFiberReactions.h
+++ micro_fo/src/bioFiberReactions.h
@@ -6,12 +6,15 @@
 #include <utility>
 namespace bio
 {
-  #define FBR_RCT_TYPES(OP) OP(linear), OP(nonlinear), OP(num_fbr_constitutive)
-  enum FiberConstitutive { FBR_RCT_TYPES(MAKE_ENUM_OP) };
+#define FBR_RCT_TYPES(OP) OP(linear), OP(nonlinear), OP(num_fbr_constitutive)
+  enum FiberConstitutive
+  {
+    FBR_RCT_TYPES(MAKE_ENUM_OP)
+  };
   const char * getFiberConstitutiveString(int ii);
   struct FiberReaction
   {
-    virtual std::pair<double,double> forceReaction(double,double) const= 0;
+    virtual std::pair<double, double> forceReaction(double, double) const = 0;
   };
   struct NonlinearReaction : public FiberReaction
   {
@@ -19,74 +22,19 @@ namespace bio
     double B;
     double E;
     double length_ratio_trns;
-    std::pair<double,double> forceReaction(double orig_length, double length) const
-    {
-      std::pair<double,double> result;
-      // Reminder: Every time you change E you have to change the derivatives of the forces too. See gfunc.c
-      double length_ratio = length / orig_length;
-      double tension = E * fiber_area / B;
-      double cmp_ratio_trns = 1.0;
-      if(length_ratio > length_ratio_trns)
-      {
-        double green_strain_trns = 0.5 * (length_ratio_trns * length_ratio_trns - 1.0);
-        double expBeps_trns = exp(B * green_strain_trns);
-        double m = fiber_area * E * length_ratio_trns * expBeps_trns; // slope
-        double b = tension * (expBeps_trns - 1.0) - m * length_ratio_trns; // y-intercept
-        result.first = m * length_ratio + b;
-        result.second = m / orig_length;
-      }
-      else if(length_ratio < cmp_ratio_trns)
-      {
-        double green_strain_trns = 0.5 * (cmp_ratio_trns * cmp_ratio_trns - 1.0);
-        double expBeps_trns = exp(B * green_strain_trns);
-        double m = fiber_area * E * cmp_ratio_trns * expBeps_trns; // slope
-        double b = tension * (expBeps_trns - 1.0) - m * cmp_ratio_trns; // y-intercept
-        result.first = m * length_ratio + b;
-        result.second = m / orig_length;
-      }
-      else
-      {
-        double green_strain = 0.5 * (length_ratio * length_ratio - 1.0);
-        double expBeps = exp(B * green_strain);
-        result.first = tension * (expBeps - 1.0);
-        result.second = ((fiber_area * E * length) / (orig_length * orig_length)) * expBeps;
-      }
-      return result;
-    }
+    std::pair<double, double> forceReaction(double orig_length,
+                                            double length) const override;
   };
   struct LinearReaction : public FiberReaction
   {
     double fiber_area;
     double E;
-    std::pair<double,double> forceReaction(double orig_length, double length) const
-    {
-      std::pair<double,double> result;
-      double length_ratio = length / orig_length;
-      result.first = ((E*fiber_area)/orig_length) * (length_ratio - 1.0);
-      result.second = (E * fiber_area) / (orig_length * orig_length);
-      return result;
-    }
-  };
-  struct LinearSupportReaction : public FiberReaction
-  {
-    double E;
-    std::pair<double,double> forceReaction(double orig_length, double length) const
-    {
-      std::pair<double,double> result;
-      double length_ratio = length / orig_length;
-      /* Remove dependence of fiber modulus on fiber area and length.
-         - modulus of support fiber is simply E
-         - F = E(l/l0-1.0)
-         - dF/dl = E/l0
-      */
-      result.first = E * (length_ratio - 1.0);
-      result.second = E / orig_length;
-      return result;
-    }
+    std::pair<double, double> forceReaction(double orig_length,
+                                            double length) const override;
   };
   struct BeamReaction : public FiberReaction
   {
-    std::pair<double,double> forceReaction(double,double) const {return std::make_pair(0.0,0.0);}
+    std::pair<double, double> forceReaction(double, double) const override;
   };
-}
+}  // namespace bio
 #endif
