9da1b41e24ebcaf54ccc0fba153f56c72ab8008e
Jacob Merson <mersoj@rpi.edu>
2020-05-20 02:06:44 -0400

    Remove simmetrix mesh utilities
    These utilities are not needed since equivalent functionality is built
    into SCOREC/core.

--- cmake/Findyaml-cpp.cmake
+++ cmake/Findyaml-cpp.cmake
@@ -1,5 +1,3 @@
-#include(util)
-
 find_package(PkgConfig REQUIRED QUIET)
 
 if(yaml-cpp_FIND_REQUIRED)
@@ -15,13 +13,9 @@ endif()
 set(ENV{PKG_CONFIG_PATH}  "${yaml-cpp_DIR}")
 
 pkg_check_modules(YAML-CPP ${_yaml-cpp_OPTS} yaml-cpp)
-#message(INFO ${yaml-cpp_INCLUDEDIR})
-#message(INFO ${yaml-cpp_INCLUDE_DIRS})
 
 if(YAML-CPP_FOUND)
   set(yaml-cpp_INCLUDE_DIRS "${YAML-CPP_INCLUDEDIR}")
-  #set(yaml-cpp_INCLUDE_DIRS "${YAML-CPP_INCLUDE_DIRS}")
-  #set(yaml-cpp_LIBRARIES "${YAML-CPP_STATIC_LDFLAGS};${YAML-CPP_STATIC_LIBRARIES}")
   set(yaml-cpp_LIBRARIES "${YAML-CPP_LDFLAGS};${YAML-CPP_LIBRARIES}")
 endif(YAML-CPP_FOUND)
 
