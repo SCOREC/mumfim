a4472c9e6f4d193812f7c96a8760fe50f17fdfda
Jacob Merson <jacob@jacobmerson.com>
2023-07-19 02:44:04 -0400

    update exception definitions

--- src/mumfim/exceptions.h
+++ src/mumfim/exceptions.h
@@ -1,33 +1,42 @@
 #ifndef MUMFIM_SRC_MUMFIM_EXCEPTIONS_H
 #define MUMFIM_SRC_MUMFIM_EXCEPTIONS_H
-#include <stdexcept>
 #include <petsc.h>
+
+#include <stdexcept>
+
 namespace mumfim
 {
-  class mumfim_error : public std::runtime_error
+  struct mumfim_error : std::runtime_error
   {
-    public:
-    explicit mumfim_error(const std::string & err) : std::runtime_error{err} {}
-    explicit mumfim_error(const char * err) : std::runtime_error{err} {}
+    using std::runtime_error::runtime_error;
   };
-  class petsc_error : public std::exception
+
+  struct material_error : mumfim_error
+  {
+    using mumfim_error::mumfim_error;
+  };
+
+  class petsc_error : public mumfim_error
   {
     public:
     explicit petsc_error(PetscErrorCode error_code)
+        : mumfim_error("Petsc Error")
     {
       const char * text;
       char * specific;
       PetscErrorMessage(error_code, &text, &specific);
       std::stringstream error_message_stream;
       error_message_stream << "Petsc Error " << error_code << "\n"
-                    << text << "\n"
-                    << specific;
+                           << text << "\n"
+                           << specific;
       error_message_ = error_message_stream.str();
     }
-    [[ nodiscard]]
-    const char * what() const noexcept override {
+
+    [[nodiscard]] const char * what() const noexcept override
+    {
       return error_message_.c_str();
     }
+
     std::string error_message_;
   };
 }  // namespace mumfim
